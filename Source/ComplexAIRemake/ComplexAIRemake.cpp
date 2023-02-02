787a87ed8c3aa1e3be7a2f7d3f0a8cf797163107
Terrick Thibodeaux <terrickthibodeaux@gmail.com>
2023-02-02 09:40:16 -0600

    Save

---
+++ Source/ComplexAIRemake/ComplexAIRemake.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ComplexAIRemake.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ComplexAIRemake, "ComplexAIRemake" );
+ 
---
+++ Source/ComplexAIRemake/ComplexAIRemakeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ComplexAIRemakeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AComplexAIRemakeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AComplexAIRemakeGameMode();
+};
+
+
+
